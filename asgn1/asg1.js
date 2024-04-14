996088365257fc02a97895af5abd46b054fb0baa
Dhriti <goomer.dhriti@gmail.com>
2024-04-13 19:41:08 -0700

    working code until draw your own part of the lab

--- asgn1/asg1.js
+++ asgn1/asg1.js
@@ -57,7 +57,8 @@ function setupWebGL() {
   canvas = document.getElementById('webgl');
 
   // Get the rendering context for WebGL
-  gl = getWebGLContext(canvas);
+  // gl = getWebGLContext(canvas);
+  gl = canvas.getContext("webgl", {preserveDrawingBuffer:true});
   if (!gl) {
     console.log('Failed to get the rendering context for WebGL');
     return;
@@ -93,8 +94,14 @@ function connectVariablesToGLSL() {
     }
 }
 
+const POINT = 0;
+const TRIANGLE = 1;
+const CIRCLE = 2;
+
 let g_selectedColor = [1.0, 1.0, 1.0, 1.0];
 let g_selectedSize = 5.0;
+let g_selectedType = POINT;
+let g_selectedSegments = 10;
 
 function addActionsForHtmlUI(){
   // button events: shape type
@@ -106,9 +113,21 @@ function addActionsForHtmlUI(){
   document.getElementById('greenSlide').addEventListener('mouseup', function() {g_selectedColor[1] = (this.value)/100; });
   document.getElementById('blueSlide').addEventListener('mouseup', function() {g_selectedColor[2] = (this.value)/100;  });
 
+  document.getElementById('segmentSlide').addEventListener('mouseup', function() {g_selectedSegments = this.value;  });
+
   // size slider events
   document.getElementById('sizeSlide').addEventListener('mouseup', function() {g_selectedSize = this.value; });
+
+  //set clear button
+  document.getElementById('clear-button').onclick = function () { g_shapesList = []; renderAllShapes(); };
+
+  // set shape selector buttons
+  document.getElementById('square-button').onclick = function() { g_selectedType = POINT; };
+  document.getElementById('triangle-button').onclick = function() { g_selectedType = TRIANGLE; };
+  document.getElementById('circle-button').onclick = function() { g_selectedType = CIRCLE; };
+
 }
+
 function main() {
   setupWebGL();
   connectVariablesToGLSL();
@@ -116,54 +135,43 @@ function main() {
 
   // Register function (event handler) to be called on a mouse press
   canvas.onmousedown = click;
+  // canvas.onmousemove = click;
+  canvas.onmousemove = function(ev) {if (ev.buttons == 1) { click(ev)}}
 
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
-  }
 
-  class Point {
-    constructor() {
-      this.type = 'point';
-      this.position = [0.0, 0.0, 0.0];
-      this.color = [1.0, 1.0, 1.0, 1.0];
-      this.size = 5.0;
-    }
-  }
+}
 
   var g_shapesList = [];
-  // var g_points = [];  // The array for the position of a mouse press
-  // var g_colors = [];  // The array to store the color of a point
-  // var g_sizes = [];   // the array to store the size of a point
 
   function click(ev) {
 
     [x, y] = convertCoordinatesEventToGL(ev);
 
-    let point = new Point();
-    point.position = [x,y];
-    point.color = g_selectedColor.slice();
-    point.size = g_selectedSize;
+    let point;
+
+    if (g_selectedType == POINT) {
+      point = new Point();
+    } else if (g_selectedType == TRIANGLE) {
+      point = new Triangle();
+    } else if (g_selectedType == CIRCLE) {
+      point = new Circle();
+      point.segments = g_selectedSegments;
+    }
+    point.position=[x,y];
+    point.color=g_selectedColor.slice();
+    point.size=g_selectedSize;
     g_shapesList.push(point);
-      // // Store the coordinates to g_points array
-    // g_points.push([x, y]);
-    // // Store the coordinates to g_points array
-    //
-    // //store the colors in colors array
-    // g_colors.push(g_selectedColor.slice());
-    //
-    // g_sizes.push(g_selectedSize);
-
-
-    // if (x >= 0.0 && y >= 0.0) {      // First quadrant
-    //   g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
-    // } else if (x < 0.0 && y < 0.0) { // Third quadrant
-    //   g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
-    // } else {                         // Others
-    //   g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
-    // }
+    // create and store new point
+    // let point = new Point();
+    // point.position = [x,y];
+    // point.color = g_selectedColor.slice();
+    // point.size = g_selectedSize;
+    // g_shapesList.push(point);
 
     renderAllShapes();
   }
@@ -179,28 +187,31 @@ function main() {
     return([x,y]);
   }
 
+
   function renderAllShapes() {
+    var startTime = performance.now();
     // Clear <canvas>
     gl.clear(gl.COLOR_BUFFER_BIT);
     var len = g_shapesList.length;
     // var len = g_points.length;
     for(var i = 0; i < len; i++) {
-      var xy = g_shapesList[i].position;
-      var rgba = g_shapesList[i].color;
-      var size = g_shapesList[i].size;
-      // var xy = g_points[i];
-      // var rgba = g_colors[i];
-      // var size = g_sizes[i];
+      g_shapesList[i].render();
 
-      // Pass the position of a point to a_Position variable
-      gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-      // Pass the color of a point to u_FragColor variable
-      gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-
-      gl.uniform1f(u_Size, size);
-      // Draw
-      gl.drawArrays(gl.POINTS, 0, 1);
     }
+
+    var duration = performance.now() - startTime;
+    sendTextToHTML("numdot: " + len + " ms: " + Math.floor(duration) + " fps: " + Math.floor(1000/duration), "numdot");
+
+
+  }
+
+  function sendTextToHTML(text, htmlID) {
+    var htmlElm = document.getElementById(htmlID);
+    if (!htmlElm) {
+      console.log("Failed to get " + htmlID + " from html");
+      return;
+    }
+    htmlElm.innerHTML = text;
   }
 
 
